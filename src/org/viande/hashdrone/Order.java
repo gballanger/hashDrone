c8bbe0599abc171af549c8692ea0bc22b88ba68e
Gilles Ballanger <gilles.ballanger@spherea.com>
2016-02-11 21:00:17 +0100

    f

--- src/org/viande/hashdrone/Order.java
+++ src/org/viande/hashdrone/Order.java
@@ -1,9 +1,16 @@
 package org.viande.hashdrone;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
+	public Integer id;
 	public Point dest;
 	public List<Integer> orders;
 	
+	public Order(Integer id, Point point) {
+		this.id = id;
+		this.dest = point;
+		this.orders = new ArrayList<Integer>();
+	}
 }
--- src/org/viande/hashdrone/Warehouse.java
+++ src/org/viande/hashdrone/Warehouse.java
@@ -1,10 +1,17 @@
 package org.viande.hashdrone;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Warehouse {
 	public Point place;
-	public List<Integer> products;
-	public Integer warehouse_id = 0;
-	public Integer nbProducts = 0;
+	public List<Integer> productsList;
+	public Integer id = 0;
+	
+	public Warehouse(Integer id, Point position)
+	{
+		this.productsList = new ArrayList<Integer>();
+		this.id = id;
+		this.place = position;
+	}
 }
