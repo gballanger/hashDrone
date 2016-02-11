64ca7653169751a42f3c16372d8ad522913dd9eb
belvis_c <clement.belvisee@epitech.eu>
2016-02-11 22:44:01 +0100

    Context & Parser : add of the parsing of Orders, warehouses, goods

--- src/org/viande/hashdrone/Context.java
+++ src/org/viande/hashdrone/Context.java
@@ -7,8 +7,12 @@ public class Context {
 	public Integer rows;
 	public Integer columns;
 	public Integer turnNumber;
+	public Integer orderNumber;
 	public Integer maxPayload;
 	public Integer droneNumber;
+	public Integer typeNumber;
+	public Integer warehouseNumber;
+	public List<Integer> goodWeight;
 	public List<Warehouse> listWharehouse;
 	public List<Drone> listDrone;
 	public List<Order> listOrder;
@@ -18,6 +22,10 @@ public class Context {
 		this.listDrone = new ArrayList<Drone>();
 		this.listWharehouse = new ArrayList<Warehouse>();
 		this.listOrder = new ArrayList<Order>();
+		this.goodWeight = new ArrayList<Integer>();
+		this.typeNumber = 0;
+		this.warehouseNumber = 0;
+		this.orderNumber = 0;
 	}
 	
 }
--- src/org/viande/hashdrone/Parser.java
+++ src/org/viande/hashdrone/Parser.java
@@ -16,15 +16,15 @@ public class Parser {
 			String line;
 			inputFile = new FileReader(fileName);
 			reader = new BufferedReader(inputFile);
-			
+			String res[];			
+			Integer iter = 0;
+			Integer jter = 0;
+			Point pos;
 
 			while ((line = reader.readLine()) != null)
 			{
 				switch (state) {
-				
-				
 				case "begin":
-					String res[];
 					state = "product";
 					res = line.split(" ");
 					context.rows = Integer.parseInt(res[0]);
@@ -34,7 +34,59 @@ public class Parser {
 					context.maxPayload = Integer.parseInt(res[4]);
 					break;
 				case "product" :
-					
+					if (context.typeNumber == 0)
+						context.typeNumber = Integer.parseInt(line);
+					else
+					{
+						state = "warehouse";
+						res = line.split(" ");
+						for (iter = 0; iter != res.length; iter++ )
+							context.goodWeight.add(Integer.parseInt(res[iter]));
+					}
+					break;
+				case "warehouse" :
+					if (context.warehouseNumber == 0)
+						{
+							context.warehouseNumber = Integer.parseInt(line);
+							iter = 0;
+						}
+					else if (iter != context.warehouseNumber)
+					{
+						pos = new Point();
+						res = line.split(" ");
+						pos.r = Integer.parseInt(res[0]);
+						pos.c = Integer.parseInt(res[1]);
+						Warehouse w = new Warehouse(iter, pos);
+								
+						line = reader.readLine();
+						res = line.split(" ");
+						for (jter = 0; jter != context.typeNumber; jter++)
+							w.productsList.set(jter, Integer.parseInt(res[jter]));
+						context.listWharehouse.add(w);
+					}
+					else
+						state = "orders";
+					break;
+				case "orders" :
+					if (context.orderNumber == 0)
+					{
+						context.orderNumber = Integer.parseInt(line);
+						iter = 0;
+					}
+					else if (iter != context.orderNumber)
+					{
+						pos = new Point();
+						res = line.split(" ");
+						pos.r = Integer.parseInt(res[0]);
+						pos.c = Integer.parseInt(res[1]);
+						Order o = new Order(iter, pos);
+						
+						line = reader.readLine();
+						res = line.split(" ");
+						for (jter = 0; jter != context.typeNumber; jter++)
+							o.orders.set(jter, Integer.parseInt(res[jter]));
+					}
+					break;
 				default:
 					break;
 				}
